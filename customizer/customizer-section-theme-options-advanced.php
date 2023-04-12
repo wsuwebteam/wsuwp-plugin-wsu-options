34b9934480f1ab44fc6abd311738b197205fffb7
Danial Bleile <danbleile@gmail.com>
2023-04-12 14:33:16 -0700

    Enhancement | Support Development Branch

--- customizer/customizer-section-theme-options-advanced.php
+++ customizer/customizer-section-theme-options-advanced.php
@@ -14,26 +14,53 @@ class Customizer_Section_Theme_Options_Advanced extends Customizer_Section {
 			static::get_option_id( 'wds', 'version' ),
 			array(
 				'capability' => 'edit_theme_options',
-				'default'    => 2,
+				'default'    => '2.x',
 				'type'       => 'option',
 			)
 		);
 
-		$wp_customize->add_control(
-			static::get_option_slug( 'wds', 'version' ),
-			array(
-				'settings'    => static::get_option_id( 'wds', 'version' ),
-				'type'        => 'select',
-				'section'     => static::$section_id,
-				'label'       => __( 'WDS Version' ),
-				'description' => __( 'Change WDS Version.' ),
-				'choices'     => array(
-					'2.x'      => 'Version 2 (recommended)',
-					'2.beta'   => 'Beta | Version 2',
-					'3.beta'   => 'Beta | Version 3',
-				),
-			)
+		$versions = array(
+			'2.x'         => 'Version 2 (recommended)',
+			'2.beta'      => 'Beta | Version 2',
+			'3.beta'      => 'Beta | Version 3',
 		);
+
+		$version = WSU_Options::get_wsu_option( 'wds', 'version', false );
+
+		if ( ! empty( $version ) && ! array_key_exists( $version, $versions ) ) {
+
+			$wp_customize->add_setting(
+				static::get_option_id( 'wds', 'version' ),
+				array(
+					'capability' => 'edit_theme_options',
+					'default'    => 2,
+					'type'       => 'option',
+				)
+			);
+
+			$wp_customize->add_control( 
+				static::get_option_slug( 'wds', 'version' ),
+				array(
+					'label'    => 'Custom Branch',
+					'section'  => static::$section_id,
+					'settings' => static::get_option_id( 'wds', 'version' ),
+					'type'     => 'text',
+				)
+			);
+
+		} else {
+			$wp_customize->add_control(
+				static::get_option_slug( 'wds', 'version' ),
+				array(
+					'settings'    => static::get_option_id( 'wds', 'version' ),
+					'type'        => 'select',
+					'section'     => static::$section_id,
+					'label'       => __( 'WDS Version' ),
+					'description' => __( 'Change WDS Version.' ),
+					'choices'     => array_merge( $versions, array( 'development' => 'Development' ) ),
+				)
+			);
+		}
 
 	}
 
