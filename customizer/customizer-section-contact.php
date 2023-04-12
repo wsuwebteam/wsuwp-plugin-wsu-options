ca15b518d929c57b817f88f8654501a948da1aa7
Danial Bleile <danbleile@gmail.com>
2023-04-12 14:32:22 -0700

    Bug Fix | Add contact back

--- customizer/customizer-section-contact.php
+++ customizer/customizer-section-contact.php
@@ -1,62 +1,53 @@
 <?php namespace WSUWP\Plugin\WSU_Options;
 
-class Section_Contact extends Customizer_Section {
-	protected $section_id    = 'wsu_contact';
-	protected $section_title = 'Contact Information';
-	protected $priority      = 170;
-	protected $permissions   = 'activate_plugins';
-	protected $description   = '';
+class Customizer_Section_Contact extends Customizer_Section {
+	protected static $section_id    = 'wsu_contact';
+	protected static $section_title = 'Contact Information';
+	protected static $priority      = 170;
+	protected static $permissions   = 'activate_plugins';
+	protected static $description   = '';
 
 
-	protected function add_controls( $wp_customize ) {
-
-		$wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'organization' ),
-			array(
-				'capability' => 'edit_theme_options',
-				'default'    => '',
-				'type'       => 'option',
-			)
-		);
-
-        $wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'address' ),
-			array(
-				'capability' => 'edit_theme_options',
-				'default'    => '',
-				'type'       => 'option',
-			)
-		);
-
-        $wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'city' ),
-			array(
-				'capability' => 'edit_theme_options',
-				'default'    => '',
-				'type'       => 'option',
-			)
-		);
-
-        $wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'state ' ),
-			array(
-				'capability' => 'edit_theme_options',
-				'default'    => '',
-				'type'       => 'option',
-			)
-		);
-
-        $wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'zip' ),
-			array(
-				'capability' => 'edit_theme_options',
-				'default'    => '',
-				'type'       => 'option',
-			)
-		);
+	protected static function add_controls( $wp_customize ) {
 
 		$wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'phone' ),
+			static::get_option_id( 'contact', 'organization' ),
+			array(
+				'capability' => 'edit_theme_options',
+				'default'    => '',
+				'type'       => 'option',
+			)
+		);
+
+        $wp_customize->add_setting(
+			static::get_option_id( 'contact', 'address' ),
+			array(
+				'capability' => 'edit_theme_options',
+				'default'    => '',
+				'type'       => 'option',
+			)
+		);
+
+        $wp_customize->add_setting(
+			static::get_option_id( 'contact', 'city' ),
+			array(
+				'capability' => 'edit_theme_options',
+				'default'    => '',
+				'type'       => 'option',
+			)
+		);
+
+        $wp_customize->add_setting(
+			static::get_option_id( 'contact', 'state ' ),
+			array(
+				'capability' => 'edit_theme_options',
+				'default'    => '',
+				'type'       => 'option',
+			)
+		);
+
+        $wp_customize->add_setting(
+			static::get_option_id( 'contact', 'zip' ),
 			array(
 				'capability' => 'edit_theme_options',
 				'default'    => '',
@@ -65,7 +56,16 @@ class Section_Contact extends Customizer_Section {
 		);
 
 		$wp_customize->add_setting(
-			$this->get_option_id( 'contact', 'email' ),
+			static::get_option_id( 'contact', 'phone' ),
+			array(
+				'capability' => 'edit_theme_options',
+				'default'    => '',
+				'type'       => 'option',
+			)
+		);
+
+		$wp_customize->add_setting(
+			static::get_option_id( 'contact', 'email' ),
 			array(
 				'capability' => 'edit_theme_options',
 				'default'    => '',
@@ -74,71 +74,71 @@ class Section_Contact extends Customizer_Section {
 		);
 
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'organization' ),
+			static::get_option_slug( 'contact', 'organization' ),
 			array(
 				'label'    => 'Organization',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'organization' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'organization' ),
 				'type'     => 'text',
 			)
 		);
 
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'address' ),
+			static::get_option_slug( 'contact', 'address' ),
 			array(
 				'label'    => 'Address',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'address' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'address' ),
 				'type'     => 'text',
 			)
 		);
 
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'city' ),
+			static::get_option_slug( 'contact', 'city' ),
 			array(
 				'label'    => 'City',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'city' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'city' ),
 				'type'     => 'text',
 			)
 		);
 
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'state' ),
+			static::get_option_slug( 'contact', 'state' ),
 			array(
 				'label'    => 'State',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'state' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'state' ),
 				'type'     => 'text',
 			)
 		);
 
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'zip' ),
+			static::get_option_slug( 'contact', 'zip' ),
 			array(
 				'label'    => 'Zip',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'zip' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'zip' ),
 				'type'     => 'text',
 			)
 		);
 		
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'phone' ),
+			static::get_option_slug( 'contact', 'phone' ),
 			array(
 				'label'    => 'Phone',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'phone' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'phone' ),
 				'type'     => 'text',
 			)
 		);
 		
         $wp_customize->add_control( 
-			$this->get_option_slug( 'contact', 'email' ),
+			static::get_option_slug( 'contact', 'email' ),
 			array(
 				'label'    => 'Email',
-				'section'  => $this->section_id,
-				'settings' => $this->get_option_id( 'contact', 'email' ),
+				'section'  => static::$section_id,
+				'settings' => static::get_option_id( 'contact', 'email' ),
 				'type'     => 'text',
 			)
 		);
